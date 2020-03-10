68fb87747a34962aad185ef7fdd41b7016b7df5a
amit206 <amit.av206@gmail.com>
2020-03-10 15:05:45 +0200

    add post

--- Plantly/NewPostViewController.swift
+++ Plantly/NewPostViewController.swift
@@ -8,23 +8,52 @@
 
 import UIKit
 
-class NewPostViewController: UIViewController {
-
+class NewPostViewController: UIViewController,
+    UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+    
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var pstText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        takePic()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func choosePic(_ sender: UIButton) {
+        takePic()
     }
-    */
+    
+    func takePic() {
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerController.SourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType =
+                UIImagePickerController.SourceType.photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    var selectedImage:UIImage?
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage;
+        self.avatar.image = selectedImage;
+        dismiss(animated: true, completion: nil);
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+        if let image = selectedImage{
+            //            postsModel.instance.saveImage(image: image) { (url) in               print("saved image url \(url)");
+            let pst = Post(id: "546", postText: self.pstText.text!, imgUrl: self.pstText.text!, uId: "2")
+            
 
+            postsModel.postsInstance.add(post: pst);
+            self.navigationController?.popViewController(animated: true);
+            //            }
+        }
+    }
+    
 }
